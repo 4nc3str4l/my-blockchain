e10a67afec6226515df20908319cf8a716eea235
4nc3str4l <cmuri_10@hotmail.com>
2023-11-30 23:16:21 +0100

    Start writting "real" signed transactions

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 )
 
 func main() {
-
+	quantity, success := new(big.Int).SetString("100000000", 10)
 	w := NewWallet()
 	log.Println(w.PrivateKeyStr())
 	log.Println(w.PublicKeyStr())
 	log.Println(w.BlockchainAddress())
 
+	t := NewUnsignedTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", quantity)
+	log.Printf("signature %s \n", t.GenerateSignature())
+
 	bc := NewBlockchain("My Address")
 	for i := 0; i < 3; i++ {
-		quantity, success := new(big.Int).SetString("100000000", 10)
+
 		if !success {
 			panic("Could not parse quantity")
 		}
--- transaction.go
+++ transaction.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/big"
 	"strings"
@@ -33,5 +37,48 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		SenderAddr:    t.senderAddr,
 		RecipientAddr: t.recipientAddr,
 		Value:         t.value,
+	})
+}
+
+/// NEw transaction logic
+
+type UnsignedTransaction struct {
+	senderPrivateKey           *ecdsa.PrivateKey
+	senderPublicKey            *ecdsa.PublicKey
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      *big.Int
+}
+
+type Signature struct {
+	R *big.Int
+	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%x%x", s.R, s.S)
+}
+
+func NewUnsignedTransaction(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey,
+	sender string, recipient string, value *big.Int) *UnsignedTransaction {
+	return &UnsignedTransaction{privateKey, publicKey, sender, recipient, value}
+}
+
+func (t *UnsignedTransaction) GenerateSignature() *Signature {
+	m, _ := json.Marshal(t)
+	h := sha256.Sum256([]byte(m))
+	r, s, _ := ecdsa.Sign(rand.Reader, t.senderPrivateKey, h[:])
+	return &Signature{r, s}
+}
+
+func (t *UnsignedTransaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Sender    string   `json:"sender_blockchain_address"`
+		Recipient string   `json:"recipient_blockchain_adddress"`
+		Value     *big.Int `json:"value"`
+	}{
+		Sender:    t.senderBlockchainAddress,
+		Recipient: t.recipientBlockchainAddress,
+		Value:     t.value,
 	})
 }
