f8abda6f67adedfbe8631d25ff478203044c70b3
4nc3str4l <cmuri_10@hotmail.com>
2023-12-01 15:12:24 +0100

    Reestructure Project

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
-import (
-	"log"
-	"math/big"
-)
+import "log"
+
+func init() {
+	log.SetPrefix("🔗 Blockchain: ")
+}
 
 func main() {
-	quantity, success := new(big.Int).SetString("100000000", 10)
-	if !success {
-		panic("Could not parse quantity")
-	}
-
-	walletMiner := NewWallet()
-	walletA := NewWallet()
-	walletB := NewWallet()
-
-	t := NewUnsignedTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), quantity)
-
-	// Blockchain
-	blockchain := NewBlockchain(walletMiner.BlockchainAddress())
-	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), quantity, walletA.PublicKey(), t.GenerateSignature())
-	log.Println("Added: ", isAdded)
-
-	blockchain.Mine()
-	blockchain.Print()
-
-	log.Printf("A %d\n", blockchain.ComputeBalance(walletA.BlockchainAddress()))
-	log.Printf("B %d\n", blockchain.ComputeBalance(walletB.BlockchainAddress()))
-	log.Printf("M %d\n", blockchain.ComputeBalance(walletMiner.BlockchainAddress()))
+	log.Println("Hello!")
 }
